4bc8df885d547efdf99b18357391fc03f8b77616
yupseth <cristinayup@gmail.com>
2022-12-03 18:21:41 +0200

    Create responsive game-display

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 // Selections
 const startButton = document.querySelector(".button--start");
+const gameDisplay = document.querySelector(".game-display");
 
 // Helper functions
 const toggleHidden = function () {
   this.classList.toggle("hidden");
+  gameDisplay.classList.remove("hidden");
 };
 
 // Events
