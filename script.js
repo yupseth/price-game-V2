ff6f177ecfd3e089d1681060f64ff39e7e234e22
yupseth <cristinayup@gmail.com>
2022-12-03 17:39:12 +0200

    Create and style start button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+// Selections
+const startButton = document.querySelector(".button--start");
+
+// Helper functions
+const toggleHidden = function () {
+  this.classList.toggle("hidden");
+};
+
+// Events
+startButton.addEventListener("click", toggleHidden);
